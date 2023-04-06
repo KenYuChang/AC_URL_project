7a9fc1dd1241ced3bbd5f163cd69b2576ec79e2d
KenYuChang <justin0915justin0915@gmail.com>
2023-04-06 12:28:23 +0800

    add mongoose & hbs

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const mongoose = require("mongoose");
+const app = express();
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); //設定連線到mongoDB
+
+const db = mongoose.connection;
+
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.listen(3000, (req, res) => {
+  console.log("this app is listening on http://localhost:3000");
+});
